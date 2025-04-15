0f6943117f7d450a83e876df8a57edd56904e0e6
Denys Rudyk <rudykdenys@gmail.com>
2025-04-15 14:48:17 +0300

    feat: add custom markers

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
 import { StyleSheet, View } from "react-native";
 
-import MapView, { Marker } from "react-native-maps";
-import MapViewCluster from "react-native-map-clustering";
+import MapView, { Marker, UrlTile } from "react-native-maps";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 const initialRegion = {
   latitude: 49.832910401276386,
@@ -11,25 +12,80 @@ const initialRegion = {
 };
 
 export default function HomeScreen() {
+  const [userLocation, setUserLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  }>({
+    latitude: 49.82880427225914,
+    longitude: 24.041275964021366,
+  });
+
+  const [selectedLocation, setSelectedLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+
+  useEffect(() => {
+    let subscription: Location.LocationSubscription | null = null;
+
+    const requestLocationPermission = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+
+        return;
+      }
+
+      subscription = await Location.watchPositionAsync(
+        { distanceInterval: 50, accuracy: Location.Accuracy.High },
+        (location) => {
+          console.log("Location: ", location);
+
+          setUserLocation({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          });
+        }
+      );
+    };
+
+    requestLocationPermission();
+
+    return () => {
+      subscription?.remove();
+    };
+  }, []);
+
+  const handleMapPress = (event: any) => {
+    const { latitude, longitude } = event.nativeEvent.coordinate;
+    setSelectedLocation({ latitude, longitude });
+  };
+
   return (
     <View style={styles.container}>
-      <MapViewCluster
+      <MapView
         style={styles.map}
-        mapType="satellite"
+        mapType="standard"
         initialRegion={initialRegion}
-        onLongPress={(e) => {
-          console.log("Coordinates", e.nativeEvent.coordinate);
-        }}
+        onPress={handleMapPress}
       >
         <Marker
-          coordinate={{
-            latitude: 49.82880427225914,
-            longitude: 24.041275964021366,
-          }}
+          coordinate={userLocation}
           title="Наша точка"
           description="Ми тут"
-        />
-      </MapViewCluster>
+        >
+          <View style={styles.currentPosition} />
+        </Marker>
+
+        {selectedLocation !== null && (
+          <Marker
+            coordinate={selectedLocation}
+            title="Вибрана точка"
+            description="Ви вибрали це місце"
+          />
+        )}
+      </MapView>
     </View>
   );
 }
@@ -42,5 +98,13 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
+  },
+  currentPosition: {
+    width: 20,
+    height: 20,
+    borderRadius: "50%",
+    backgroundColor: "#19F",
+    borderColor: "#fff",
+    borderWidth: 2,
   },
 });
